e2daabc3a0bc1ff8f7333e4e5a26637ca3cf7d33
Cathal McCabe <cathalmccabe@Cathals-MBP.home>
2020-03-12 11:11:00 +0000

    Updates

---
+++ precache-manifest.bacb29fae8e68af0745a6316cac4d54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b6c68e2632cc285d8d5718629e4c01",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8d7aaaa43f2f54f55f2d",
+    "url": "/monsters-rolodex/static/css/main.8ba2c69c.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7aaaa43f2f54f55f2d",
+    "url": "/monsters-rolodex/static/js/main.c6d82e7f.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
